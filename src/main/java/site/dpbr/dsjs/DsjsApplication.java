9de676395e1376f75d4a34d2ed3fc84e6eed3b46
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-14 00:32:38 +0900

    Initial commit

---
+++ src/main/java/site/dpbr/dsjs/DsjsApplication.java
@@ -0,0 +1,13 @@
+package site.dpbr.dsjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsjsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/dpbr/dsjs/DsjsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dpbr.dsjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
