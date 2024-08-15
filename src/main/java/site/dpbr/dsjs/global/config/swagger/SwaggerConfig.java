4a86ad9db3b0bf77357492e833acad84ec2a8b6c
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-15 23:59:09 +0900

    feat: swagger설정

---
+++ src/main/java/site/dpbr/dsjs/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package site.dpbr.dsjs.global.config.swagger;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@OpenAPIDefinition(info = @Info(
+        title = "단생조사 API",
+        description = "단생조사 API 문서",
+        version = "v1.0.0"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openApi() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        Server server = new Server();
+        server.setUrl("/");
+
+        return new OpenAPI()
+                .servers(List.of(server))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
