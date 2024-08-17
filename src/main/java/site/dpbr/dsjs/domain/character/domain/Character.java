0c38c70bd9a052fbdf16a30e8846bc3573013126
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-17 18:06:03 +0900

    feat: `@Getter` 추가

--- src/main/java/site/dpbr/dsjs/domain/character/domain/Character.java
+++ src/main/java/site/dpbr/dsjs/domain/character/domain/Character.java
@@ -3,10 +3,12 @@ package site.dpbr.dsjs.domain.character.domain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Getter
 @Entity(name = "characters")
 @AllArgsConstructor
 @NoArgsConstructor
