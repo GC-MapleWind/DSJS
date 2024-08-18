d571e1e1381f25a6288f737fbd9593e151829b0b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-19 00:15:16 +0900

    feat: 컨트롤러에 캐릭터의 유니온 정보를 불러오는 로직 추가

--- src/main/java/site/dpbr/dsjs/domain/character/presentation/CharacterController.java
+++ src/main/java/site/dpbr/dsjs/domain/character/presentation/CharacterController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import site.dpbr.dsjs.domain.character.presentation.dto.request.CharacterOcidRequest;
 import site.dpbr.dsjs.domain.character.usecase.FetchBasicInfo;
 import site.dpbr.dsjs.domain.character.usecase.FetchOcid;
+import site.dpbr.dsjs.domain.character.usecase.FetchUnionInfo;
 import site.dpbr.dsjs.domain.character.usecase.UploadCharacterList;
 import site.dpbr.dsjs.global.error.ErrorResponse;
 
@@ -25,6 +26,7 @@ public class CharacterController {
     private final FetchOcid fetchOcid;
     private final UploadCharacterList uploadCharacterList;
     private final FetchBasicInfo fetchBasicInfo;
+    private final FetchUnionInfo fetchUnionInfo;
 
 
     @Operation(summary = "캐릭터 목록 엑셀 파일 업로드", description = "캐릭터 목록 엑셀 파일을 DB에 저장합니다.")
@@ -65,5 +67,18 @@ public class CharacterController {
     @PatchMapping("/basic")
     public ResponseEntity<String> fetchBasicInfo() throws IOException {
         return ResponseEntity.ok(fetchBasicInfo.execute());
+    }
+
+    @Operation(summary = "캐릭터의 유니온 정보 호출", description = "Nexon Open API를 통해 캐릭터의 유니온 정보를 호출합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PatchMapping("/union")
+    public ResponseEntity<String> fetchUnionInfo() throws IOException {
+        return ResponseEntity.ok(fetchUnionInfo.execute());
     }
 }
