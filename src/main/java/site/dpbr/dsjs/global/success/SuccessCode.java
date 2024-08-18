ca807b028f54067894540dffa0ed8eec30c188b9
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-19 00:14:53 +0900

    feat: 캐릭터의 유니온 정보를 불러오는 성공 처리 구현

--- src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
+++ src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
     UPLOAD_FILE_SUCCESS(201, "UPLOAD_FILE_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 가져오는데 성공했습니다."),
-    FETCH_BASIC_INFO_SUCCESS(200, "FETCH_BASIC_INFO_SUCCESS", "캐릭터의 기본 정보를 가져오는데 성공했습니다.");
+    FETCH_BASIC_INFO_SUCCESS(200, "FETCH_BASIC_INFO_SUCCESS", "캐릭터의 기본 정보를 가져오는데 성공했습니다."),
+    FETCH_UNION_INFO_SUCCESS(200, "FETCH_UNION_INFO_SUCCESS", "캐릭터의 유니온 정보를 가져오는데 성공했습니다.");
 
     private final int httpStatus;
     private final String code;
