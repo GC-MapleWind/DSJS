6438e44843262f8c5953c5c96c2695bf0e616dbc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-18 23:03:49 +0900

    feat: 캐릭터의 기본 정보를 불러오는 성공 처리 구현

--- src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
+++ src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-    UPLOAD_FILE_SUCCESS(201, "UPLOAD_FILE_SUCCESS", "파일에 있는 캐릭터들의 정보를 가져오는데 성공했습니다.");
+    UPLOAD_FILE_SUCCESS(201, "UPLOAD_FILE_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 가져오는데 성공했습니다."),
+    FETCH_BASIC_INFO_SUCCESS(200, "FETCH_BASIC_INFO_SUCCESS", "캐릭터의 기본 정보를 가져오는데 성공했습니다.");
 
     private final int httpStatus;
     private final String code;
