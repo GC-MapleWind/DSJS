f78107a6cc147e2568a68c4723763b86effff4ab
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-25 21:38:04 +0900

    feat: 캐릭터의 무릉도장 정보를 불러오는 성공 처리 구현

--- src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
+++ src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum SuccessCode {
     UPLOAD_FILE_SUCCESS(201, "UPLOAD_FILE_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 가져오는데 성공했습니다."),
     FETCH_BASIC_INFO_SUCCESS(200, "FETCH_BASIC_INFO_SUCCESS", "캐릭터의 기본 정보를 가져오는데 성공했습니다."),
+    FETCH_MULUNG_INFO_SUCCESS(200, "FETCH_MULUNG_INFO_SUCCESS", "캐릭터의 무릉도장 정보를 가져오는데 성공했습니다."),
     FETCH_STAT_INFO_SUCCESS(200, "FETCH_STAT_INFO_SUCCESS", "캐릭터의 스탯 정보를 가져오는데 성공했습니다."),
     FETCH_UNION_INFO_SUCCESS(200, "FETCH_UNION_INFO_SUCCESS", "캐릭터의 유니온 정보를 가져오는데 성공했습니다.");
 
