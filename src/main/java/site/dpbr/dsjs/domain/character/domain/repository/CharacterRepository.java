9a09aacdbbda5d53f5eda1e8c66fc36ba1d895d1
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-03 13:04:15 +0900

    feat: DB에서 캐릭터의 정보를 검색하기 위한 로직 구현

--- src/main/java/site/dpbr/dsjs/domain/character/domain/repository/CharacterRepository.java
+++ src/main/java/site/dpbr/dsjs/domain/character/domain/repository/CharacterRepository.java
@@ -3,10 +3,13 @@ package site.dpbr.dsjs.domain.character.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.dpbr.dsjs.domain.character.domain.Character;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CharacterRepository extends JpaRepository<Character, UUID> {
 
     Optional<Character> findByName(String characterName);
+
+    List<Character> findAllByOrderByCombatPowerDesc();
 }
---
+++ src/main/java/site/dpbr/dsjs/domain/character/usecase/SearchCharacterInfo.java
@@ -0,0 +1,28 @@
+package site.dpbr.dsjs.domain.character.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.dpbr.dsjs.domain.character.domain.Character;
+import site.dpbr.dsjs.domain.character.domain.repository.CharacterRepository;
+import site.dpbr.dsjs.domain.character.exception.CharacterNotFoundException;
+import site.dpbr.dsjs.domain.character.presentation.dto.response.CharacterInfoResponse;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SearchCharacterInfo {
+    private final CharacterRepository characterRepository;
+
+    public CharacterInfoResponse execute(String characterName) {
+        Character character = characterRepository.findByName(characterName).orElseThrow(CharacterNotFoundException::new);
+
+        List<Character> characters = characterRepository.findAllByOrderByCombatPowerDesc();
+        int size = characters.size();
+        int ranking = characters.indexOf(character) + 1;
+
+        double percentage = ranking * 100.0 / size;
+
+        return CharacterInfoResponse.from(character, percentage);
+    }
+}
