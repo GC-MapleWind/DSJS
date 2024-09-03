14d3130a9c56312f1a340720494acbac00e47821
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-03 18:07:28 +0900

    feat: 컨트롤러에 DB에 있는 모든 캐릭터 정보를 일괄 업데이트하는 로직 추가

--- src/main/java/site/dpbr/dsjs/domain/character/presentation/CharacterController.java
+++ src/main/java/site/dpbr/dsjs/domain/character/presentation/CharacterController.java
@@ -26,6 +26,7 @@ public class CharacterController {
     private final ExportCharacterListToExcel exportCharacterListToExcel;
     private final ExportCharacterImages exportCharacterImages;
     private final SearchCharacterInfo searchCharacterInfo;
+    private final UpdateAllCharacterInfo updateAllCharacterInfo;
 
     @Operation(summary = "캐릭터 목록 엑셀 파일 업로드 및 전체 정보 로드", description = "캐릭터 목록 엑셀 파일을 DB에 저장하고, Nexon Open API를 통해 필요한 모든 정보를 호출합니다.")
     @ApiResponses({
@@ -89,5 +90,18 @@ public class CharacterController {
     @GetMapping(value = "/search")
     public ResponseEntity<SearchCharacterInfoResponse> searchCharacterInfo(@RequestParam String characterName) {
         return ResponseEntity.ok(searchCharacterInfo.execute(characterName));
+    }
+
+    @Operation(summary = "캐릭터 정보 업데이트", description = "DB에 저장된 모든 캐릭터의 정보를 업데이트 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PatchMapping(value = "/update")
+    public ResponseEntity<String> updateAllCharacterInfo() {
+        return ResponseEntity.ok(updateAllCharacterInfo.execute());
     }
 }
