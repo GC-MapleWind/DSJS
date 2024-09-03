55684a4caa04d8973b865cde06ecfdef1b644961
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-03 13:18:31 +0900

    remove: 불필요한 로직 삭제

--- src/main/java/site/dpbr/dsjs/domain/character/presentation/CharacterController.java
+++ src/main/java/site/dpbr/dsjs/domain/character/presentation/CharacterController.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import site.dpbr.dsjs.domain.character.presentation.dto.request.CharacterOcidRequest;
 import site.dpbr.dsjs.domain.character.presentation.dto.response.CharacterInfoResponse;
 import site.dpbr.dsjs.domain.character.usecase.*;
 import site.dpbr.dsjs.global.error.ErrorResponse;
@@ -23,15 +22,10 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class CharacterController {
 
-    private final FetchOcid fetchOcid;
     private final UploadAndFetchInfo uploadAndFetchInfo;
     private final ExportCharacterListToExcel exportCharacterListToExcel;
     private final ExportCharacterImages exportCharacterImages;
     private final SearchCharacterInfo searchCharacterInfo;
-    private final FetchBasicInfoFromCharacterList fetchBasicInfoFromCharacterList;
-    private final FetchUnionInfoFromCharacterList fetchUnionInfoFromCharacterList;
-    private final FetchStatInfoFromCharacterList fetchStatInfoFromCharacterList;
-    private final FetchMuLungInfoFromCharacterList fetchMuLungInfoFromCharacterList;
 
     @Operation(summary = "캐릭터 목록 엑셀 파일 업로드 및 전체 정보 로드", description = "캐릭터 목록 엑셀 파일을 DB에 저장하고, Nexon Open API를 통해 필요한 모든 정보를 호출합니다.")
     @ApiResponses({
@@ -95,71 +89,5 @@ public class CharacterController {
     @GetMapping(value = "/search")
     public ResponseEntity<CharacterInfoResponse> searchCharacterInfo(@RequestParam String characterName) {
         return ResponseEntity.ok(searchCharacterInfo.execute(characterName));
-    }
-
-    @Operation(summary = "캐릭터의 ocid 호출", description = "Nexon Open API를 통해 캐릭터의 ocid를 호출합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @GetMapping("/ocid")
-    public ResponseEntity<String> fetchOcid(CharacterOcidRequest request) throws IOException {
-        String characterName = request.characterName();
-        return ResponseEntity.ok(fetchOcid.execute(characterName));
-    }
-
-    @Operation(summary = "캐릭터의 기본 정보 호출", description = "Nexon Open API를 통해 캐릭터의 기본 정보를 호출합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PatchMapping("/basic")
-    public ResponseEntity<String> fetchBasicInfo() throws IOException {
-        return ResponseEntity.ok(fetchBasicInfoFromCharacterList.execute());
-    }
-
-    @Operation(summary = "캐릭터의 스텟 정보 호출", description = "Nexon Open API를 통해 캐릭터의 스텟 정보를 호출합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PatchMapping("/stat")
-    public ResponseEntity<String> fetchStatInfo() throws IOException {
-        return ResponseEntity.ok(fetchStatInfoFromCharacterList.execute());
-    }
-
-    @Operation(summary = "캐릭터의 유니온 정보 호출", description = "Nexon Open API를 통해 캐릭터의 유니온 정보를 호출합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PatchMapping("/union")
-    public ResponseEntity<String> fetchUnionInfo() throws IOException {
-        return ResponseEntity.ok(fetchUnionInfoFromCharacterList.execute());
-    }
-
-    @Operation(summary = "캐릭터의 무릉도장 정보 호출", description = "Nexon Open API를 통해 캐릭터의 무릉도장 정보를 호출합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PatchMapping("/mulung")
-    public ResponseEntity<String> fetchMuLungInfo() throws IOException {
-        return ResponseEntity.ok(fetchMuLungInfoFromCharacterList.execute());
     }
 }
--- src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
+++ src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
@@ -6,12 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-    UPLOAD_FILE_SUCCESS(201, "UPLOAD_FILE_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 저장하는데 성공했습니다."),
-    UPLOAD_AND_FETCH_SUCCESS(201, "UPLOAD_AND_FETCH_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 가져오는데 성공했습니다."),
-    FETCH_BASIC_INFO_SUCCESS(200, "FETCH_BASIC_INFO_SUCCESS", "캐릭터의 기본 정보를 가져오는데 성공했습니다."),
-    FETCH_MULUNG_INFO_SUCCESS(200, "FETCH_MULUNG_INFO_SUCCESS", "캐릭터의 무릉도장 정보를 가져오는데 성공했습니다."),
-    FETCH_STAT_INFO_SUCCESS(200, "FETCH_STAT_INFO_SUCCESS", "캐릭터의 스탯 정보를 가져오는데 성공했습니다."),
-    FETCH_UNION_INFO_SUCCESS(200, "FETCH_UNION_INFO_SUCCESS", "캐릭터의 유니온 정보를 가져오는데 성공했습니다.");
+    UPLOAD_AND_FETCH_SUCCESS(201, "UPLOAD_AND_FETCH_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 불러오고 DB에 저장하는데 성공했습니다.");
 
     private final int httpStatus;
     private final String code;
