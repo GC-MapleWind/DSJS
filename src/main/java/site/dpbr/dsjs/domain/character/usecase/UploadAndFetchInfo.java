de5b5e92b54cde306b785be8dcdf6e1985061c4b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-03 13:12:19 +0900

    fix: 부정확한 병렬처리 로직 삭제

--- src/main/java/site/dpbr/dsjs/domain/character/usecase/UploadAndFetchInfo.java
+++ src/main/java/site/dpbr/dsjs/domain/character/usecase/UploadAndFetchInfo.java
@@ -23,12 +23,6 @@ import site.dpbr.dsjs.global.success.SuccessCode;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
@@ -38,70 +32,54 @@ public class UploadAndFetchInfo {
     private final ObjectMapper objectMapper;
     private final Connection connection;
     private final CharacterRepository characterRepository;
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public String execute(String date, MultipartFile file) {
         if (file.isEmpty()) {
             throw new EmptyFileException();
         }
 
-        try (InputStream inputStream = file.getInputStream();
-             Workbook workbook = new XSSFWorkbook(inputStream)) {
+        try (InputStream inputStream = file.getInputStream()) {
+            Workbook workbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = workbook.getSheetAt(0); // 첫 번째 시트를 가져옵니다.
 
-            Sheet sheet = workbook.getSheetAt(0);
-            Iterator<Row> rowIterator = sheet.iterator();
-
-            // 비동기 작업을 담을 CompletableFuture 리스트
-            List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-            rowIterator.forEachRemaining(row -> {
-                Iterator<Cell> cellIterator = row.iterator();
-                if (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
+            // 엑셀 데이터 처리
+            for (Row row : sheet) {
+                for (Cell cell : row) {
                     String characterName = cell.getStringCellValue();
 
-                    if (!characterName.isEmpty()) {
-                        // 비동기 작업을 CompletableFuture로 감싸고, 스레드 풀을 사용하여 병렬 처리
-                        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-                            try {
-                                String ocid = fetchOcid.execute(characterName);
-
-                                String path = "/character/basic?ocid=" + ocid + "&date=" + date;
-                                CharacterBasicInfoResponse characterBasicInfoResponse = objectMapper.readValue(connection.execute(path),
-                                        CharacterBasicInfoResponse.class);
-
-                                path = "/character/stat?ocid=" + ocid + "&date=" + date;
-                                CharacterStatInfoResponse characterStatInfoResponse = objectMapper.readValue(connection.execute(path),
-                                        CharacterStatInfoResponse.class);
-
-                                path = "/user/union?ocid=" + ocid + "&date=" + date;
-                                CharacterUnionInfoResponse characterUnionInfoResponse = objectMapper.readValue(connection.execute(path),
-                                        CharacterUnionInfoResponse.class);
-
-                                path = "/character/dojang?ocid=" + ocid + "&date=" + date;
-                                CharacterMuLungInfoResponse characterMuLungInfoResponse = objectMapper.readValue(connection.execute(path),
-                                        CharacterMuLungInfoResponse.class);
-
-                                Character character = Character.create(ocid, characterName);
-                                character.updateBasicInfo(characterBasicInfoResponse);
-                                character.updateStatInfo(characterStatInfoResponse);
-                                character.updateUnionInfo(characterUnionInfoResponse);
-                                character.updateMuLungInfo(characterMuLungInfoResponse);
-
-                                characterRepository.save(character);
-                            } catch (IOException e) {
-                                throw new ServiceException(ErrorCode.INTERNAL_SERVER_ERROR);
-                            }
-                        }, executorService);
-
-                        futures.add(future); // 작업 리스트에 추가
+                    if(characterName.isEmpty()) {
+                        continue;
                     }
+
+                    String ocid = fetchOcid.execute(characterName);
+
+                    String path = "/character/basic?ocid=" + ocid + "&date=" + date;
+                    CharacterBasicInfoResponse characterBasicInfoResponse = objectMapper.readValue(connection.execute(path),
+                            CharacterBasicInfoResponse.class);
+
+                    path = "/character/stat?ocid=" + ocid + "&date=" + date;
+                    CharacterStatInfoResponse characterStatInfoResponse = objectMapper.readValue(connection.execute(path),
+                            CharacterStatInfoResponse.class);
+
+                    path = "/user/union?ocid=" + ocid + "&date=" + date;
+                    CharacterUnionInfoResponse characterUnionInfoResponse = objectMapper.readValue(connection.execute(path),
+                            CharacterUnionInfoResponse.class);
+
+                    path = "/character/dojang?ocid=" + ocid + "&date=" + date;
+                    CharacterMuLungInfoResponse characterMuLungInfoResponse = objectMapper.readValue(connection.execute(path),
+                            CharacterMuLungInfoResponse.class);
+
+                    Character character = Character.create(ocid, characterName);
+                    character.updateBasicInfo(characterBasicInfoResponse);
+                    character.updateStatInfo(characterStatInfoResponse);
+                    character.updateUnionInfo(characterUnionInfoResponse);
+                    character.updateMuLungInfo(characterMuLungInfoResponse);
+
+                    characterRepository.save(character);
                 }
-            });
+            }
 
-            // 모든 비동기 작업이 완료될 때까지 대기
-            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-
+            workbook.close();
         } catch (IOException e) {
             throw new ServiceException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
