ba17284443b55c8bee1e0579fc27942ede98c521
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-03 18:05:23 +0900

    feat: DB에 있는 모든 캐릭터 정보 일괄 업데이트 성공 처리 구현

--- src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
+++ src/main/java/site/dpbr/dsjs/global/success/SuccessCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-    UPLOAD_AND_FETCH_SUCCESS(201, "UPLOAD_AND_FETCH_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 불러오고 DB에 저장하는데 성공했습니다.");
+    UPLOAD_AND_FETCH_SUCCESS(201, "UPLOAD_AND_FETCH_SUCCESS", "엑셀 파일에 있는 캐릭터들의 정보를 불러오고 DB에 저장하는데 성공했습니다."),
+    UPDATE_SUCCESS(201, "UPDATE_SUCCESS", "모든 캐릭터의 정보를 업데이트하는데 성공했습니다.");
 
     private final int httpStatus;
     private final String code;
