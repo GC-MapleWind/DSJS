a0b6259434f1404aa2833bbe1653e2fa610a000e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-10 17:50:38 +0900

    feat: 예외 처리 시 에러 로깅 기능 추가

--- src/main/java/site/dpbr/dsjs/global/error/exception/GlobalExceptionHandler.java
+++ src/main/java/site/dpbr/dsjs/global/error/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package site.dpbr.dsjs.global.error.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,18 +8,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.dpbr.dsjs.global.error.ErrorCode;
 import site.dpbr.dsjs.global.error.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    public static final String SERVICE_EXCEPTION_LOG_FORMAT = "ServiceException occurred: {}";
+    public static final String UNEXPECTED_ERROR_LOG_FORMAT = "Unexpected error occurred: {}";
 
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ErrorResponse> handleServiceException(ServiceException e) {
+        log.error(SERVICE_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getHttpStatus()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception e) {
-        e.printStackTrace();
+        log.error(UNEXPECTED_ERROR_LOG_FORMAT, e.getMessage(), e);
+
         ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/site/dpbr/dsjs/global/error/exception/ServiceException.java
+++ src/main/java/site/dpbr/dsjs/global/error/exception/ServiceException.java
@@ -1,11 +1,14 @@
 package site.dpbr.dsjs.global.error.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import site.dpbr.dsjs.global.error.ErrorCode;
 
 @Getter
-@AllArgsConstructor
 public class ServiceException extends RuntimeException {
     private final ErrorCode errorCode;
+
+     public ServiceException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 }
