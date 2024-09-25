84da83bde1f9bed83b62025feaa44a58a701d49f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-25 17:42:05 +0900

    refactor: 요구사항 변경에 따른 응답 DTO 수정

--- src/main/java/site/dpbr/dsjs/domain/character/presentation/dto/response/SearchCharacterInfoResponse.java
+++ src/main/java/site/dpbr/dsjs/domain/character/presentation/dto/response/SearchCharacterInfoResponse.java
@@ -3,30 +3,38 @@ package site.dpbr.dsjs.domain.character.presentation.dto.response;
 import site.dpbr.dsjs.domain.character.domain.Character;
 
 public record SearchCharacterInfoResponse(
-        String name,
-        String gender,
-        String world,
-        String job,
-        Integer level,
-        Integer unionLevel,
-        Integer unionArtifactLevel,
-        Long combatPower,
-        Integer muLungFloor,
-        String characterImage,
-        Double percentage
+	String name,
+	String gender,
+	String world,
+	String job,
+	Integer level,
+	Integer unionLevel,
+	Long combatPower,
+	String characterImage,
+	Integer charactersCount,
+	Integer sameWorldCount,
+	Integer sameJobCount,
+	Double levelRanking,
+	Double unionRanking,
+	Double combatPowerRanking
+
 ) {
-    public static SearchCharacterInfoResponse of(Character character, Double percentage) {
-        return new SearchCharacterInfoResponse(
-                character.getName(),
-                character.getGender(),
-                character.getWorld(),
-                character.getJob(),
-                character.getLevel(),
-                character.getUnionLevel(),
-                character.getUnionArtifactLevel(),
-                character.getCombatPower(),
-                character.getMuLungFloor(),
-                character.getCharacterImage(),
-                percentage);
-    }
+	public static SearchCharacterInfoResponse of(Character character, Integer charactersCount, Integer sameWorldCount,
+		Integer sameJobCount, Double levelRanking, Double unionRanking, Double combatPowerRanking) {
+		return new SearchCharacterInfoResponse(
+			character.getName(),
+			character.getGender(),
+			character.getWorld(),
+			character.getJob(),
+			character.getLevel(),
+			character.getUnionLevel(),
+			character.getCombatPower(),
+			character.getCharacterImage(),
+			charactersCount,
+			sameWorldCount,
+			sameJobCount,
+			levelRanking,
+			unionRanking,
+			combatPowerRanking);
+	}
 }
