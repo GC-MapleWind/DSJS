58d1fec15f012cfdb451b125e3e2500f28580066
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-09-25 17:40:19 +0900

    feat: 같은 월드 수, 같은 직업 수, 레벨 순위, 유니온 순위 추가

--- src/main/java/site/dpbr/dsjs/domain/character/domain/repository/CharacterRepository.java
+++ src/main/java/site/dpbr/dsjs/domain/character/domain/repository/CharacterRepository.java
@@ -1,15 +1,24 @@
 package site.dpbr.dsjs.domain.character.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import site.dpbr.dsjs.domain.character.domain.Character;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import site.dpbr.dsjs.domain.character.domain.Character;
+
 public interface CharacterRepository extends JpaRepository<Character, UUID> {
 
-    Optional<Character> findByName(String characterName);
+	Optional<Character> findByName(String characterName);
 
-    List<Character> findAllByOrderByCombatPowerDesc();
+	List<Character> findAllByWorld(String world);
+
+	List<Character> findAllByJob(String job);
+
+	List<Character> findAllByOrderByCombatPowerDesc();
+
+	List<Character> findAllByOrderByLevelDesc();
+
+	List<Character> findAllByOrderByUnionLevelDesc();
 }
--- src/main/java/site/dpbr/dsjs/domain/character/usecase/SearchCharacterInfo.java
+++ src/main/java/site/dpbr/dsjs/domain/character/usecase/SearchCharacterInfo.java
@@ -1,28 +1,38 @@
 package site.dpbr.dsjs.domain.character.usecase;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 import site.dpbr.dsjs.domain.character.domain.Character;
 import site.dpbr.dsjs.domain.character.domain.repository.CharacterRepository;
 import site.dpbr.dsjs.domain.character.exception.CharacterNotFoundException;
 import site.dpbr.dsjs.domain.character.presentation.dto.response.SearchCharacterInfoResponse;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class SearchCharacterInfo {
-    private final CharacterRepository characterRepository;
+	private final CharacterRepository characterRepository;
 
-    public SearchCharacterInfoResponse execute(String characterName) {
-        Character character = characterRepository.findByName(characterName).orElseThrow(CharacterNotFoundException::new);
+	public SearchCharacterInfoResponse execute(String characterName) {
+		Character character = characterRepository.findByName(characterName)
+			.orElseThrow(CharacterNotFoundException::new);
 
-        List<Character> characters = characterRepository.findAllByOrderByCombatPowerDesc();
-        int size = characters.size();
-        int ranking = characters.indexOf(character) + 1;
+		List<Character> characters = characterRepository.findAllByOrderByCombatPowerDesc();
+		int charactersCount = characters.size();
 
-        double percentage = ranking * 100.0 / size;
+		int sameWorldCount = characterRepository.findAllByWorld(character.getWorld()).size();
+		int sameJobCount = characterRepository.findAllByJob(character.getJob()).size();
 
-        return SearchCharacterInfoResponse.of(character, percentage);
-    }
+		double levelRanking =
+			(characterRepository.findAllByOrderByLevelDesc().indexOf(character) + 1) * 100.0 / charactersCount;
+		double unionRanking =
+			(characterRepository.findAllByOrderByUnionLevelDesc().indexOf(character) + 1) * 100.0 / charactersCount;
+
+		double combatPowerRanking = characters.indexOf(character) * 100.0 / charactersCount;
+
+		return SearchCharacterInfoResponse.of(character, charactersCount, sameWorldCount, sameJobCount, levelRanking,
+			unionRanking, combatPowerRanking);
+	}
 }
